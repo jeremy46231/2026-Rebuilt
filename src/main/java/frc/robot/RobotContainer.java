d0adfcae11febf630cb159738dc4d8f7fe1448f8
Tim Shmidt <timsh007@gmail.com>
2026-01-12 20:44:12 -0800

    bugfix w default command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -48,7 +48,7 @@ public class RobotContainer {
         new InstantCommand(
             () -> {
               lebron.stopAll();
-            }));
+            }, lebron));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
