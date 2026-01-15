307dc863f2e5907a6fdcebf69a03a5fe45c5c93c
arj2cool <arjunv.lzp@gmail.com>
2026-01-14 18:37:06 -0800

    changed names

--- src/main/java/frc/robot/commands/DriveToPose.java
+++ src/main/java/frc/robot/commands/DriveToPose.java
@@ -10,15 +10,15 @@ import java.util.function.Supplier;
 /** This Command drives the robot in a linear path to a specific pose. */
 public class DriveToPose extends Command {
   @SuppressWarnings("PMD.UnusedPrivateField")
-  private final CommandSwerveDrivetrain m_swerve;
+  private final CommandSwerveDrivetrain swerve;
 
   // Initialize the LinearPath and the LinearPath.State to a null value
-  private LinearPath m_path = null;
-  private LinearPath.State m_pathState = null;
+  private LinearPath path = null;
+  private LinearPath.State pathState = null;
 
   // Initialize the Target Pose and the Target Pose Supplier to a null value
-  private Pose2d m_targetPose = null;
-  private Supplier<Pose2d> m_targetPoseSupplier = null;
+  private Pose2d targetPose = null;
+  private Supplier<Pose2d> targetPoseSupplier = null;
 
   /**
    * @param swerve Swerve Subsystem.
@@ -26,8 +26,8 @@ public class DriveToPose extends Command {
    */
   public DriveToPose(CommandSwerveDrivetrain swerve, Pose2d targetPose) {
     // Use addRequirements() here to declare subsystem dependencies.
-    m_swerve = swerve;
-    m_targetPose = targetPose;
+    this.swerve = swerve;
+    this.targetPose = targetPose;
 
     addRequirements(swerve);
   }
@@ -38,9 +38,9 @@ public class DriveToPose extends Command {
    */
   public DriveToPose(CommandSwerveDrivetrain swerve, Supplier<Pose2d> targetPoseSupplier) {
     // Use addRequirements() here to declare subsystem dependencies.
-    m_swerve = swerve;
-    m_targetPoseSupplier = targetPoseSupplier;
-    m_targetPose = m_targetPoseSupplier.get();
+    this.swerve = swerve;
+    this.targetPoseSupplier = targetPoseSupplier;
+    this.targetPose = targetPoseSupplier.get();
 
     addRequirements(swerve);
   }
@@ -48,20 +48,20 @@ public class DriveToPose extends Command {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    m_path =
+    path =
         new LinearPath(
             new TrapezoidProfile.Constraints(1, 1), new TrapezoidProfile.Constraints(0.2, 0.2));
-    m_pathState =
-        new LinearPath.State(m_swerve.getCurrentState().Pose, m_swerve.getCurrentState().Speeds);
+    pathState =
+        new LinearPath.State(swerve.getCurrentState().Pose, swerve.getCurrentState().Speeds);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if (m_pathState != null) {
-      m_pathState = m_path.calculate(3.0, m_pathState, m_targetPose);
+    if (pathState != null) {
+      pathState = path.calculate(3.0, pathState, targetPose);
 
-      m_swerve.applyFieldSpeeds(m_pathState.speeds);
+      swerve.applyFieldSpeeds(pathState.speeds);
     }
   }
 
@@ -72,8 +72,8 @@ public class DriveToPose extends Command {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    if (m_path.isFinished(5)) {
-      m_pathState = null;
+    if (path.isFinished(5)) {
+      pathState = null;
       return true;
     }
     return false;
