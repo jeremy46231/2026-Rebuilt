3ea556866afd00793bd16ed600e766d3c76c5ae0
73anthonyL <73anthony.lu@gmail.com>
2026-01-22 18:48:01 -0800

    commit from EOD Jan 21 (unfinished work)

--- src/main/java/frc/robot/util/ActivityTimer.java
+++ src/main/java/frc/robot/util/ActivityTimer.java
@@ -1,7 +1,25 @@
 package frc.robot.util;
 
+import java.util.function.BooleanSupplier;
+
+import com.ctre.phoenix6.Utils;
+
 import edu.wpi.first.wpilibj.DriverStation;
 
 public class ActivityTimer {
-    
+    private double teleopStartTime;
+    private boolean secondActiveAlliance;
+    //= Utils.getCurrentTimeSeconds();
+
+    public ActivityTimer() {
+        teleopStartTime = Utils.getCurrentTimeSeconds();
+    }
+
+    void setAllianceOrdering(BooleanSupplier secondActiveAlliance) {
+        this.secondActiveAlliance = secondActiveAlliance.getAsBoolean();
+    }
+
+    boolean getActiveHubAlliance() {
+
+    } 
 }
--- src/main/java/frc/robot/util/MiscUtils.java
+++ src/main/java/frc/robot/util/MiscUtils.java
@@ -1,12 +1,39 @@
 package frc.robot.util;
 
+import java.util.Optional;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.DriverStation;
 
 public class MiscUtils {
   public static Pose2d plus(Pose2d a, Translation2d b) {
     return new Pose2d(
         a.getX() + b.getX(), a.getY() + b.getY(), new Rotation2d(a.getRotation().getRadians()));
+  }
+
+  public static Optional<Boolean> pullSecondActiveCharacter() {
+    Optional<Boolean> ret;
+    String gameData;
+    gameData = DriverStation.getGameSpecificMessage();
+
+    if (gameData.length() > 0) {
+      switch (gameData.charAt(0)) {
+        case 'B' :
+          ret = Optional.of(false);
+          break;
+        case 'R' :
+          ret = Optional.of(true);
+          break;
+        default :
+          ret = Optional.empty();
+          break;
+      }
+    }
+    else {
+      ret = Optional.empty();
+    }
+    return ret;
   }
 }
