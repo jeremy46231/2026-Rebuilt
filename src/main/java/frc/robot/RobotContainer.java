0cae971c2b51b3c81d060b3f62cec740cd3ba4d0
73anthonyL <73anthony.lu@gmail.com>
2026-01-24 13:37:59 -0800

    fixed issue with default command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -74,7 +74,8 @@ public class RobotContainer {
     //             () ->
     //                 point.withModuleDirection(
     //                     new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
-    intake.setDefaultCommand(new InstantCommand(() -> intake.setProportion(0.0)));
+    joystick.a().whileTrue(new InstantCommand(() -> intake.setProportion(0.0)));
+
     joystick.b().whileTrue(new InstantCommand(() -> intake.setProportion(-1.0)));
     // // Run SysId routines when holding back/start and X/Y.
     // // Note that each routine should be run exactly once in a single log.
