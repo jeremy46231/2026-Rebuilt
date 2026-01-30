805448560cd70a4cfd547573069a5047732c7abb
73anthonyL <73anthony.lu@gmail.com>
2026-01-29 19:56:33 -0800

    added joystick command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -47,6 +47,20 @@ public class RobotContainer {
   private void configureBindings() {
     // Note that X is defined as forward according to WPILib convention,
     // and Y is defined as to the left according to WPILib convention.
+    // drivetrain.setDefaultCommand(
+    //     // Drivetrain will execute this command periodically
+    //     drivetrain.applyRequest(
+    //         () ->
+    //             drive
+    //                 .withVelocityX(
+    //                     -joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+    //                 .withVelocityY(
+    //                     -joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+    //                 .withRotationalRate(
+    //                     -joystick.getRightX()
+    //                         * MaxAngularRate) // Drive counterclockwise with negative X (left)
+    //         ));
+
     drivetrain.setDefaultCommand(
         // Drivetrain will execute this command periodically
         drivetrain.applyRequest(
