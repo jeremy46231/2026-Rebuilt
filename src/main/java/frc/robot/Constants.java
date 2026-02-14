a1714f158bf59b7c21d94cccad728e355fd28536
Tim Shmidt <timsh007@gmail.com>
2026-02-14 10:06:38 -0800

    tuning

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -525,7 +525,7 @@ public final class Constants {
 
     public static final boolean SHOOTS_BACKWARDS = false;
 
-    public static final double ANGULAR_TOLERANCE_FOR_AUTO_AIM_RAD = .1;
+    public static final double ANGULAR_TOLERANCE_FOR_AUTO_AIM_RAD = .2;
 
     public static final int TARGETING_CALCULATION_PRECISION = 5;
   }
