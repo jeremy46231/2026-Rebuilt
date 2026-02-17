eb89582d5b6465a593aa1afe9516a922f22c50d7
arj2cool <arjunv.lzp@gmail.com>
2026-02-17 15:36:40 -0800

    started autoroutine code

--- src/main/java/frc/robot/AutoRoutines.java
+++ src/main/java/frc/robot/AutoRoutines.java
@@ -4,17 +4,54 @@ import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.HopperSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
 
 public class AutoRoutines {
-  final AutoTrajectory moveForward;
-  final AutoRoutine routine;
+  private final AutoFactory factory;
+  private final IntakeSubsystem intake;
+  private final ShooterSubsystem lebron;
+  private final HopperSubsystem hopper;
 
-  public AutoRoutines(AutoFactory factory) {
-    routine = factory.newRoutine("CristianoRonaldo.chor");
-    moveForward = routine.trajectory("MoveForward.traj");
 
-    routine.active().onTrue(moveForward.resetOdometry().andThen(moveForward.cmd()));
+  public AutoRoutines(AutoFactory factory, IntakeSubsystem intake, ShooterSubsystem lebron, HopperSubsystem hopper) {
+    this.factory = factory;
+    this.intake = intake;
+    this.lebron = lebron;
+    this.hopper = hopper;
   }
+  
+  private AutoTrajectory maneuver(AutoRoutine routine, String name) {
+    AutoTrajectory traj;
+
+    switch (name) {
+      case "LeftManeuverL":
+        traj = routine.trajectory("LeftManeuverL");
+        break;
+      case "LeftManeuverR":
+        traj = routine.trajectory("LeftManeuverR");
+        break;
+      case "RightManeuverL":
+        traj = routine.trajectory("RightManeuverL");
+        break;
+      case "RightManeuverR":
+        traj = routine.trajectory("RightManeuverR");
+        break;
+      default:
+        traj = null;
+        break;
+    }
+
+    return traj;
+  }
+
+  private AutoTrajectory 
+
+
+
+
+
 
   public AutoRoutine moveForwardAuto() {
     return routine;
