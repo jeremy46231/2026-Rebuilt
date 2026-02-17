a195a29c24fb2792a16837e6a542d09cbc79e527
Sid Senthil <sidsenthil2010@gmail.com>
2026-02-16 19:42:38 -0800

    Rebuilt

--- src/main/java/frc/robot/commandGroups/ExpelToFullyRetract.java
+++ src/main/java/frc/robot/commandGroups/ExpelToFullyRetract.java
@@ -14,7 +14,9 @@ public class ExpelToFullyRetract extends SequentialCommandGroup {
       HopperSubsystem hopperSubsystem,
       FuelGaugeDetection fuelGaugeDetection) {
     addCommands(
-        shooterSubsystem.shootAtSpeedCommand().until(() -> hopperSubsystem.isHopperSufficientlyEmpty(fuelGaugeDetection)),
+        shooterSubsystem
+            .shootAtSpeedCommand()
+            .until(() -> hopperSubsystem.isHopperSufficientlyEmpty(fuelGaugeDetection)),
         intakeSubsystem.armToDegrees(Constants.Intake.Arm.ARM_POS_RETRACTED));
   }
 }
