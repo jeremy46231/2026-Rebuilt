aa4f3203246d14da5239ae248427acb8b0846435
Tim Shmidt <timsh007@gmail.com>
2026-02-16 21:29:21 -0800

    made an example of the vision for auto

--- src/main/java/frc/robot/AutoRoutines.java
+++ src/main/java/frc/robot/AutoRoutines.java
@@ -1,19 +1,38 @@
 package frc.robot;
 
+import java.util.ArrayList;
+
 import choreo.auto.AutoFactory;
 import choreo.auto.AutoRoutine;
 import choreo.auto.AutoTrajectory;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class AutoRoutines {
   final AutoTrajectory moveForward;
   final AutoRoutine routine;
+  private SequentialCommandGroup cmdgrp;
 
-  public AutoRoutines(AutoFactory factory) {
+  public AutoRoutines(AutoFactory factory, boolean choice1, boolean choice2) {
     routine = factory.newRoutine("CristianoRonaldo.chor");
     moveForward = routine.trajectory("MoveForward.traj");
 
-    routine.active().onTrue(moveForward.resetOdometry().andThen(moveForward.cmd()));
+    SequentialCommandGroup cmdgrp = new SequentialCommandGroup();
+    //TODO: do this but like good actually
+    cmdgrp.addCommands(routine.trajectory("PushForward").cmd());
+    if (choice1) {
+      cmdgrp.addCommands(routine.trajectory("rIntake").cmd());
+    } else {
+      cmdgrp.addCommands(routine.trajectory("lIntake").cmd());
+    }
+
+    if (choice2) {
+      cmdgrp.addCommands(routine.trajectory("rSkeddadleBack").cmd());
+    } else {
+      cmdgrp.addCommands(routine.trajectory("lSkeddadleBack").cmd());
+    }
+
+    routine.active().onTrue(moveForward.resetOdometry().andThen(cmdgrp));
   }
 
   public AutoRoutine moveForwardAuto() {
@@ -21,6 +40,6 @@ public class AutoRoutines {
   }
 
   public Command getPathAsCommand() {
-    return routine.cmd();
+    return cmdgrp;
   }
 }
