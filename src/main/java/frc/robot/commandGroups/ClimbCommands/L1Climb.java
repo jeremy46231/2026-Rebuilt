ac4fe75765d3a70ee91f8da45256c5e82902e5cc
Kalashvenku <Kalashvenku@gmail.com>
2026-02-18 21:13:59 -0800

    beginning of L1 climb fixes; incomplete.

--- src/main/java/frc/robot/commandGroups/ClimbCommands/L1Climb.java
+++ src/main/java/frc/robot/commandGroups/ClimbCommands/L1Climb.java
@@ -2,14 +2,16 @@ package frc.robot.commandGroups.ClimbCommands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
+import frc.robot.commands.DriveToPose;
 import frc.robot.subsystems.ClimberSubsystem;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class L1Climb extends SequentialCommandGroup {
   public L1Climb(ClimberSubsystem climberSubsystem, CommandSwerveDrivetrain swerveDrivetrain) {
     addCommands(
-        climberSubsystem.PullUpCommand(Constants.Climber.PullUp.L1_REACH_POS),
         climberSubsystem.SitUpCommand(Constants.Climber.SitUp.SIT_UP_ANGLE),
-        climberSubsystem.PullUpCommand(Constants.Climber.PullUp.PULL_DOWN_POS));
+        climberSubsystem.PullUpCommand(Constants.Climber.PullUp.L1_REACH_POS),
+        DriveToPose(swerveDrivetrain, () -> Constants.Landmarks.RED_TOWER),
+        climberSubsystem.PullUpCommand(Constants.Climber.PullUp.PULL_DOWN_POS_FOR_L1));
   }
 }
